216028dc2cbd353ab4bf202bae64d2252280d80e
sonalsoni10 <sonalsoni10@gmail.com>
2024-07-09 23:09:01 +0530

    changes by sonal

--- src/test/java/branchforSonal/logOutTestCase.java
+++ src/test/java/branchforSonal/logOutTestCase.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class logOutTestCase {
 	@Test(invocationCount = 3)
-	
+	//changes by sonal
 	public void method1() { 
  		System.out.println("demo2method1"); 
  		 
